2c34c3706fd5910dc58caf27b9a36d0ac1584ea6
Vinayaksharma17 <vvsharma999@gmail.com>
2023-08-11 23:51:00 +0530

    completed game logic

--- src/Components/GameContainer.jsx
+++ src/Components/GameContainer.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import GameDisplay from "./GameDisplay";
 import arrayShuffle from "array-shuffle";
 
-const types = ["❤", "🌝", "🕉", "💥", "👦", "🤩", "🌮", "🛺"];
+const types = ["📚", "🎵", "🕉", "💥", "🔥", "😎", "🌍", "🚀"];
 const getGameData = () => {
   //get types and load the types in an new array x2(as an object)
   const gameData = [];
@@ -25,22 +25,79 @@ const getGameData = () => {
   return gameData;
 };
 const GameContainer = () => {
-  const [cards, setCards] = useState(getGameData());
+  const [cards, setCards] = useState(getGameData())
+  const [inPlay, setInPlay] = useState([])
+  const [matching, setMatching] = useState(false) 
+  const [score, setScore] = useState(0)
+  const [moves, setMoves] = useState(0)
+  // inplay - array of 2 cards
 
   const handleOnClick = (id) => () => {
-    //find the card
+    if(!matching){
+      
+ //find the card
     //set the open true or false
     const updatedCards = [...cards];
+    const updatedInPlay = [...inPlay]
 
     updatedCards.forEach((card) => {
       if (card.id === id) {
         card.open = true;
+        updatedInPlay.push(card.type)
+        setInPlay(updatedInPlay)
+        // add to inplay array
       }
     });
-
     setCards(updatedCards);
-  };
-  return <GameDisplay cards={cards} onClick={handleOnClick} />;
+    // update inplay state - 1, 2
+    }
+   
+  }
+
+  useEffect(()=>{
+    // inplay === length is 2?
+    if (inPlay.length === 2 && !matching){
+      setMoves(moves + 1)
+      setMatching(true)
+      const updatedCards = [...cards]
+      // type of card 1 === type of card 2
+      if (inPlay[0] === inPlay[1]){
+        updatedCards.forEach(card => {
+          if (card.type === inPlay[0]){
+            card.remove = true
+          }
+        })
+        setCards(updatedCards)
+        setMatching(false)
+        setScore(score + 1)
+        setInPlay([])
+        //match
+      } else{
+        // no-match
+        setTimeout(()=>{
+          updatedCards.forEach(card => {
+            if (card.type === inPlay[0] || card.type === inPlay[1]){
+              card.open = false
+            }
+          })
+          setCards(updatedCards)
+          setMatching(false)
+          setInPlay([])
+        }, 1000)
+      }
+    }
+        // remove these cards (keep them open)
+        // score incrementing
+        // close the cards
+        // on operation
+  })
+  return (<GameDisplay 
+            cards={cards} 
+            onClick={handleOnClick} 
+            score ={score}
+            moves ={moves}
+            />
+  );
 };
 
 export default GameContainer;
--- src/Components/GameDisplay.jsx
+++ src/Components/GameDisplay.jsx
@@ -1,17 +1,39 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Grid, Container, Box, Button } from "@mui/material";
 import GameCard from "./GameCard";
-const GameDisplay = ({ cards, index, onClick }) => {
+const GameDisplay = ({ cards, onClick, score, moves}) => {
   const handleClick = () => {};
   return (
     <Grid container spacing={1}>
       {cards.map((card) => {
         return (
-          <Grid item xs={3}>
-            <GameCard card={card} index={index} onClick={onClick} />
+          <Grid item xs={3} key={card.id}>
+            <GameCard card={card} onClick={onClick} />
           </Grid>
         );
       })}
+      <Grid item></Grid>
+        <Grid item xs={12}>
+          <Container
+          sx={{
+            padding: 1,
+            border: 1,
+            borderRadius: 1,
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-around'
+          }}
+          
+          >
+            <Box sx={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+              Moves: {moves}
+            </Box>
+            <Button>Start Over</Button>
+            <Box sx={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+              Score: {score}
+            </Box>
+          </Container>
+        </Grid>
     </Grid>
   );
 };
